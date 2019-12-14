1dae01875815afee3b354ce9e7b2bc27efd58823
Matthew Crepea <crepea@snares-02.cs.wisc.edu>
2019-12-14 16:29:21 -0600

    Changes to makefile

--- hisat2_server_src/Executables.h
+++ hisat2_server_src/Executables.h
@@ -4,18 +4,12 @@
 // Hack-ey way of holding const values for all of our executables that we will be loading in / referencing.
 // But hey, it is what it is.
 
-extern const char *HISAT2_SMALL_PATH;
-extern const char *HISAT2_LARGE_PATH;
-extern const char *SAMTOOLS_PATH; // lol @ this path
-
-extern const char *BASIC_BACKEND_EXEC;
-extern const char *APPEND_FILTER_EXEC;
+extern const char *SAMTOOLS_PATH; 
 
 extern const char *STREAMING_BACKEND_EXEC;
 extern const char *STREAMING_MERGE_FILTER_SO;
 
 extern const char *HG38_INDEX_PATH;
 extern const char *EXAMPLE_INDEX_PATH;
-
 
 #endif /* hisat2_mrnet_utils_h */
